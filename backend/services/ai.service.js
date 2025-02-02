4e086bf4279a1a828060f034a82e310d87dd6a51
Rudra Malvankar <rudracmalvankar@gmail.com>
2025-02-02 12:33:28 +0530

    AI Message backend

--- backend/services/ai.service.js
+++ backend/services/ai.service.js
@@ -1,4 +1,6 @@
 import { GoogleGenerativeAI } from "generative-ai";
+import dotenv from "dotenv";
+dotenv.config();
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
